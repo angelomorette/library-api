7a97efd615d4e84c63423a1105e9291ba5f8d25c
Angelo <angelomorette@gmail.com>
2024-01-14 19:24:17 -0300

    feat(books): Adiciona a configuração para a conexão usando o Mongoose e conclui a configuração das operações de criação e busca

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
+// import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './interface/book.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class BooksService {
+  constructor(
+    @Inject('BOOK_MODEL')
+    private bookModel: Model<Book>
+  ) { }
+
   create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+    const createBook = new this.bookModel(createBookDto)
+    return createBook.save()
   }
 
-  findAll() {
-    return `This action returns all books`;
+  findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} book`;
+  // }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
-  }
+  // update(id: number, updateBookDto: UpdateBookDto) {
+  //   return `This action updates a #${id} book`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} book`;
+  // }
 }
--- src/books/interface/book.interface.ts
+++ src/books/interface/book.interface.ts
@@ -1,1 +1,10 @@
-export class Book {}
+import { Document } from 'mongoose';
+
+export interface Book extends Document {
+  readonly title: string;
+  readonly author: string;
+  readonly publicationYear: number;
+  readonly genre: string;
+  readonly availableCopies: number;
+  readonly isAvailable: boolean;
+}
